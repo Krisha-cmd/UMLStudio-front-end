32ff380b27103f8156556876682516a14379b365
Krisha-cmd <krishaaggarwal.mit@gmail.com>
2025-11-13 15:32:59 +0530

    Done

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import { AuthProvider } from "./context/AuthContext";
 import { ProjectProvider } from "./context/ProjectContext";
 import { DiagramProvider } from "./context/DiagramContext";
 import { Toaster } from "react-hot-toast";
+import GlobalClickEffect from "./components/GlobalClickEffect";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
       <ProjectProvider>
         <DiagramProvider>
+        <GlobalClickEffect />
           <App />
           <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
         </DiagramProvider>
